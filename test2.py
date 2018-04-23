ddf81dcda8da938b20a3f280b1f210e3f2e2e3b8
Nan Wang <nanw0321@rhel6-64e.slac.stanford.edu>
2018-04-23 12:28:57 -0700

    Add existing file

---
+++ test2.py
@@ -0,0 +1,8 @@
+import sys
+sys.argv = []
+
+def print_usage():
+    print "\nUsage: {0} <output file base name> <input file name>".format(sys.argv[0])
+    print "Arguments: "
+    print '\t-h: this help message'
+    print
