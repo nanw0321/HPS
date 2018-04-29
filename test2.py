5c1692b8a98e23235e65a22d68ba4f683831c571
nanw0321 <nanw0321@gmail.com>
2018-04-29 14:26:13 -0700

    Add existing file

--- test2.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.argv = []
-
-def print_usage():
-    print "\nUsage: {0} <output file base name> <input file name>".format(sys.argv[0])
-    print "Arguments: "
-    print '\t-h: this help message'
-    print
